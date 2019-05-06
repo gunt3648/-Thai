ddf24dd8e0b0c58661d0877c02ee8e39d7f4ef1e
Gunt Chanmas <gunt4826@outlook.com>
2019-05-06 23:11:31 +0700

    build app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBkoLHbyBJk_WazZkwrCCthv4B3AHnNk7k',
+    authDomain: 'atthai-a950a.firebaseapp.com',
+    databaseURL: 'https://atthai-a950a.firebaseio.com',
+    projectId: 'atthai-a950a',
+    storageBucket: 'atthai-a950a.appspot.com',
+    messagingSenderId: '50706982926'
+  }
 };
